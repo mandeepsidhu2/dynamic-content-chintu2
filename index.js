56c43b006273833c8785ee0af9693e682207673d
Mandeep <mandeep@Mandeeps-MacBook-Pro.local>
2021-08-12 13:35:39 +0530

    aws push working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
     res.send('Dynamic content for Chintu 2.0!')
@@ -9,3 +11,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`)
 });
+
+const awsService = require('./src/service/aws.js');
+
+app.post('/post', async (req, res, next) => {
+    try {
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).send('No files were uploaded.');
+        }
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        await awsService.uploadFile(req.files.file);
+        res.status(Status.OK).json(await Success(result))
+    } catch (e) {
+        next(e)
+    }
+})
---
+++ src/service/aws.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const util = require('util')
+const ID = 'AKIAUPH5WFXZUXB27I7W';
+const SECRET = '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';
+const BUCKET_NAME = 'hackathon-chintu2-dynamic-content';
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+    const uploadFile = async (fileName) => {
+        const writeFileAsync = util.promisify(fs.writeFile)
+        await writeFileAsync(`./${fileName.name}`, fileName.data)
+        const fileContent = fs.readFileSync(fileName.name,'utf8');
+
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: fileName.name,
+            Body: fileContent,
+            ACL:'public-read'
+        };
+
+        s3.upload(params, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+    }
+
+exports.uploadFile = uploadFile;
+
