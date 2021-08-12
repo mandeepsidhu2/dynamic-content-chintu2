2238de039c00fbe754dc043b77445a08d10c82ce
Mandeep <mandeep@Mandeeps-MacBook-Pro.local>
2021-08-12 23:26:15 +0530

    tenor

--- dynamic-video/tenor.js
+++ dynamic-video/tenor.js
@@ -1,21 +1,12 @@
-const https = require('https');
+const axios = require('axios');
 
 async function getTenorGifUrl(emotion) {
-    https.get('https://g.tenor.com/v1/search?q='+emotion+'&key=LIVDSRZULEL&limit=8',  (res) => {
-        var body = '';
-
-        res.on('data', function(chunk){
-            body += chunk;
-        });
-
-        res.on('end', function(){
-            const response = JSON.parse(body);
-            return response.results[0].media[0].gif.url
-
-        });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+    try {
+        const response = await axios.get('https://g.tenor.com/v1/search?q='+emotion+'&key=LIVDSRZULEL&limit=8');
+        return response.data.results[0].media[0].gif.url
+    } catch (error) {
+        console.error(error);
+    }
 }
 module.exports = {
     getTenorGifUrl
