3903520ff982a15cde31c764704b20dc59771531
Mandeep <mandeep@Mandeeps-MacBook-Pro.local>
2021-08-12 16:54:21 +0530

    aws upload working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const app = express();
 const port = 8000;
 const fileUpload = require('express-fileupload');
@@ -68,8 +69,8 @@ app.post('/post', async (req, res, next) => {
             return res.status(400).send('No files were uploaded.');
         }
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        await awsService.uploadFile(req.files.file);
-        res.status(Status.OK).json(await Success(result))
+        const link = await awsService.uploadFile(req.files.file);
+        res.status(200).json(link)
     } catch (e) {
         next(e)
     }
